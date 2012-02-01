053388c1feccaa4f236244b74b3634b75955e271
James Michael <jamesamichael@gmail.com>
2012-02-01 20:11:38 +0000

    Added basic getters/setters to Job class

--- Simulation of Tag Based Cooperation/src/Job.java
+++ Simulation of Tag Based Cooperation/src/Job.java
@@ -39,4 +39,48 @@ public class Job
     protected double donationRates[];
     protected Object logs[];
 
+    public Job()
+    {
+    
+    }
+
+    public double contextInfluence()
+    {
+        return contextInfluence;
+    }
+
+    public void setContextInfluence(double contextInfluence)
+    {
+        this.contextInfluence = contextInfluence;
+    }
+
+    public RewireStrategy rewireStrategy()
+    {
+        return rewireStrategy;
+    }
+
+    public void setRewireStrategy(RewireStrategy strategy)
+    {
+        rewireStrategy = strategy;
+    }
+
+    public NumberOfPairings numberOfParings()
+    {
+        return numberOfParings;
+    }
+
+    public void setNumberOfPairings(NumberOfPairings pairings)
+    {
+        numberOfParings = pairings;
+    }
+
+    public PopulationSize populationSize()
+    {
+        return populationSize;
+    }
+
+    public void setPopulationSize(PopulationSize size)
+    {
+        populationSize = size;
+    }
 }
