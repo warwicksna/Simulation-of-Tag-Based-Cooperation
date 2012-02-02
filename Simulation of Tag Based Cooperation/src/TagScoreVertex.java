466d6a1af1062d77e682e10714fdf6f41ef67a5b
James Michael <jamesamichael@gmail.com>
2012-02-02 13:17:46 +0000

    Changed TagScoreVertex to use rewire proportion for calculating the number of nodes to rewire

--- Simulation of Tag Based Cooperation/src/TagScoreVertex.java
+++ Simulation of Tag Based Cooperation/src/TagScoreVertex.java
@@ -183,7 +183,9 @@ public class TagScoreVertex extends AbstractVertex
 	
 	protected void rewire()
 	{
-		int lambda = graph.get().job().lambda();
+        int numberOfNeighbours = graph.get().neighboursForVertex(vertexId).size();
+        double rewireProportion = graph.get().job().rewireProportion();
+        int lambda = Math.round(((double) numberOfNeighbours) * rewireProportion);
 
         switch (graph.get().job().rewireStrategy())
         {
