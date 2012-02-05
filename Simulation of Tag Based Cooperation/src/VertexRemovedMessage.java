d9d79cd44da02554cda4fa631c78836b9d023da4
James Michael <jamesamichael@gmail.com>
2012-02-05 16:57:07 +0000

    Added VertexRemovedMessage class

---
+++ Simulation of Tag Based Cooperation/src/VertexRemovedMessage.java
@@ -0,0 +1,14 @@
+public class VertexRemovedMessage extends AbstractMessage
+{
+    protected String vertexId;
+
+    public VertexRemovedMessage(AbstractVertex vertex)
+    {
+        vertexId = vertex.vertexId();
+    }
+
+    public String toString()
+    {
+        return "VertexRemoved " + vertexId;
+    }
+}
