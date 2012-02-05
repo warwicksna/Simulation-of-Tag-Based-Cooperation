b9f19d4dd71d7cc6e393a6988a006948f37275e7
James Michael <jamesamichael@gmail.com>
2012-02-05 16:55:20 +0000

    Added VertexAddedMessage class

---
+++ Simulation of Tag Based Cooperation/src/VertexAddedMessage.java
@@ -0,0 +1,18 @@
+public class VertexAddedMessage extends AbstractMessage
+{
+    protected String vertexId;
+    protected double tag;
+    protected double tolerance;
+
+    public VertexAddedMessage(TagScoreVertex vertex)
+    {
+        vertexId = vertex.vertexId();
+        tag = vertex.tag();
+        tolerance = vertex.tolerance();
+    }
+
+    public String toString()
+    {
+        return "VertexAdded " + vertexId + " " + tag + " " + tolerance;
+    }
+}
